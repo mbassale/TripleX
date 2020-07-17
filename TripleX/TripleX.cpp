a0dd53d0761b94a36e6eb57bc3a0d741fe748ab1
Marco Bassaletti <bassaletti@gmail.com>
2020-07-16 23:08:52 -0400

    Basic number guessing gameplay.

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,9 +1,39 @@
-// TripleX.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "Enter the correct code to continue..." << std::endl;
+
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << std::endl;
+    std::cout << "- There are 3 numbers in the code" << std::endl;
+    std::cout << "- The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "- The codes multiply to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cout << "> Enter 3 codes: ";
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
+    {
+        std::cout << "You win!" << std::endl;
+    }
+    else 
+    {
+        std::cout << "You lose!" << std::endl;
+    }
+
     return 0;
 }
